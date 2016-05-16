a73b792d21a252b5928d8a1291a5a6e29f99d6a4
Mikel Garcia <mikel_6@hotmail.com>
2016-05-16 10:15:56 +0200

    Created Hobby model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516072847) do
+ActiveRecord::Schema.define(version: 20160516081212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20160516072847) do
   end
 
   add_index "groups", ["house_id"], name: "index_groups_on_house_id", using: :btree
+
+  create_table "hobbies", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "houses", force: :cascade do |t|
     t.string   "latitude"
