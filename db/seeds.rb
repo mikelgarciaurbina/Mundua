c29594c5ef65486fc2d44b1dcc9e9b5392a9c0e8
Mikel Garcia <mikel_6@hotmail.com>
2016-05-16 10:13:29 +0200

    Fill seed with Users, Houses and Groups

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user1 = User.new(
+      :name                  => "Mikel",
+      :email                 => "mikel@ironhack.com",
+      :password              => "123456",
+      :password_confirmation => "123456"
+  )
+user1.save!
+
+user2 = User.new(
+      :name                  => "Alex",
+      :email                 => "alex@ironhack.com",
+      :password              => "123456",
+      :password_confirmation => "123456"
+  )
+user2.save!
+
+puts "Users created"
+
+group1 = Group.create(name: "Ironhackers", description: "Los mejores Ironhacker de la historia")
+group2 = Group.create(name: "Teamhackers", description: "Los mejores Teamhacker de la historia")
+
+puts "Groups created"
+
+house1 = House.create(latitude: "40.438785", longitude: "-3.698664", address: "Calle Vargas, 3", rooms: 4, description: "sahdosahfodhf oahdsoifh oidshfoihasdoif hoadsfo hiodhsoifhoiadhs")
+house2 = House.create(latitude: "40.438781", longitude: "-3.681349", address: "Calle de Núñez de Balboa, 127", rooms: 10, description: "sahdosahfodhf oahdsoifh oidshfoihasdoif hoadsfo hiodhsoifhoiadhs")
+
+puts "Houses created"
+
+group1.house = house1
+group1.save!
+group2.house = house2
+group2.save!
+
+user1.group = group1
+user1.save!
+user2.group = group2
+user2.save!
+
+puts "Relations created"
