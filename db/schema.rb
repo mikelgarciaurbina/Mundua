875c5cb7d0d2bb7ad675d39c9537965add90068c
Mikel Garcia <mikel_6@hotmail.com>
2016-05-16 10:37:14 +0200

    Technology Model created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516082026) do
+ActiveRecord::Schema.define(version: 20160516083614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 20160516082026) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "owner_id"
+  end
+
+  create_table "technologies", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_hobbies", force: :cascade do |t|
